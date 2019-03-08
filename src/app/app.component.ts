e8f8acde2d598cf5277db7ce78ec93946ca1a871
rashikanandapattanaik <rashikanandapattanaik@gmail.com>
2019-03-08 18:02:38 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import * as Highcharts from 'highcharts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'demo1';
+  Highcharts = Highcharts; // required
+  chartConstructor = 'chart'; // optional string, defaults to 'chart'
+  chartOptions =   {
+    "title": { "text": "Highcharts chart" },
+    "series": [{
+      "data": [11,2,3],
+      "zones": [{
+        "value": 7.2,
+        "dashStyle": "dot",
+        "color": "red"
+      }]
+    }, {
+      "data": [5,6,7]
+    }]
+  }; // required
+  // chartCallback = function (chart) { ... } // optional function, defaults to null
+  updateFlag = false; // optional boolean
+  oneToOneFlag = true; // optional boolean, defaults to false
+
 }
