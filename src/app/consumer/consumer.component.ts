8f34119ef178ca1fa2df4e1f60fa70fec67444b7
Ved Prakash <ved740@gmail.com>
2019-03-09 09:41:16 +0530

    tile link

--- src/app/consumer/consumer.component.ts
+++ src/app/consumer/consumer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-consumer',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./consumer.component.css']
 })
 export class ConsumerComponent implements OnInit {
-  
+  @Input('name') masterName: string;
   constructor() { }
 
   ngOnInit() {
