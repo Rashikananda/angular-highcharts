57c31fcdd81b6476495961818414b2ddaf7f661a
Milan Meva <milanmeva@yahoo.com>
2019-03-09 12:24:02 +0530

    moment fix

--- src/app/service/data.service.ts
+++ src/app/service/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient, HttpParams } from "@angular/common/http";
-import * as moment from "moment";
+let moment = require("moment");
 import { API_ENDPOINT } from "./Constants";
 
 @Injectable({
