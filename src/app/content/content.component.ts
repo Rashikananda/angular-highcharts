95f8abfd7a015785e25ebdf370c49b650daf468b
Milan Meva <milanmeva@yahoo.com>
2019-03-09 09:47:15 +0530

    added functionality

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { DataService } from "../service/data.service";
+import { deepStrictEqual } from "assert";
 
 @Component({
-  selector: 'app-content',
-  templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  selector: "app-content",
+  templateUrl: "./content.component.html",
+  styleUrls: ["./content.component.css"]
 })
 export class ContentComponent implements OnInit {
-
-  constructor() { }
+  constructor(private ds: DataService) {}
 
   ngOnInit() {
+    this.ds.get("topic-ar.json", null).subscribe(
+      d => {
+        console.log(d);
+      },
+      err => {
+        console.log(err);
+      }
+    );
   }
-
 }
