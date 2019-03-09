b6762e6e9b1f0751a3f73c1d70c8b21b07d85ef2
Milan Meva <milanmeva@yahoo.com>
2019-03-09 11:19:53 +0530

    fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,22 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-import { HighChartsComponent } from './highCharts/high-charts/high-charts.component';
-import { ConsumersComponent } from './consumers/consumers.component';
-import { TopicsComponent } from './topics/topics.component';
-import { CommonComponent } from './common/common.component';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { HighChartsComponent } from "./highCharts/high-charts/high-charts.component";
+import { ConsumersComponent } from "./consumers/consumers.component";
+import { TopicsComponent } from "./topics/topics.component";
+import { CommonComponent } from "./common/common.component";
 // import { CommonHighChartsComponent } from './highCharts/common-high-charts/common-high-charts';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/highCharts', pathMatch: 'full' },
-  { path: 'highCharts', component: HighChartsComponent },
-  { path: 'consumers', component: ConsumersComponent },
-  { path: 'consumers/:consumerId', component: CommonComponent },
-  { path: 'topics', component: TopicsComponent },
-  { path: 'topics/:topicId', component: CommonComponent }
+  { path: "", redirectTo: "/highCharts", pathMatch: "full" },
+  { path: "highCharts", component: HighChartsComponent },
+  { path: "consumers", component: ConsumersComponent },
+  { path: "consumers/:id", component: CommonComponent },
+  { path: "topics", component: TopicsComponent },
+  { path: "topics/:id", component: CommonComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/common/common.component.ts
+++ src/app/common/common.component.ts
@@ -1,173 +1,179 @@
-import { Component, OnInit, Input, OnChanges } from '@angular/core';
-import * as Highcharts from 'highcharts';
+import { Component, OnInit, Input, OnChanges } from "@angular/core";
+import * as Highcharts from "highcharts";
 import Data from "../../assets/arrival-rate-raw.js";
 import Data1 from "../../assets/new-arraival-rate";
-import * as moment from 'moment';
-import { DataService } from '../service/data.service.js';
-import { isUndefined } from 'util';
-import { Router, ActivatedRoute, ParamMap } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import * as moment from "moment";
+import { DataService } from "../service/data.service.js";
+import { isUndefined } from "util";
+import { Router, ActivatedRoute, ParamMap } from "@angular/router";
+import { switchMap } from "rxjs/operators";
 
 @Component({
-  selector: 'app-common',
-  templateUrl: './common.component.html',
-  styleUrls: ['./common.component.css']
+  selector: "app-common",
+  templateUrl: "./common.component.html",
+  styleUrls: ["./common.component.css"]
 })
-export class CommonComponent implements  OnInit,OnChanges {
-  @Input ('name') name;
-  
-seriesData;
-topicsArray;
-consumerArray;
-// name="d dsvbdshbvhjvhdvs";
-constructor(private ds: DataService,  private route: ActivatedRoute,
-    private router: Router) {}
+export class CommonComponent implements OnInit, OnChanges {
+  @Input("name") name;
 
-name$;
-masterName;
-ngOnInit(): void {
-  console.log(this.name)
-  this.topicsArray = Array.from(new Set(Data.map(rData => rData.topic)));
-  this.topicsArray = Array.from(new Set(Data.map(rData => rData.consumer))).sort();
-  // this.seriesData = this.getProcessedArrivalData1(Data);
-  let data1;
-  this.name$ = this.route.paramMap.pipe(
-    switchMap((params: ParamMap) =>{
-       let x = params.get('id');
-       console.log(x);
-       return this.ds.get(x+".json",null);
-    })).subscribe(data=>{
-    
-        this.seriesData = this.getProcessedArrivalData2(data)
-        this.chartOptions.series = this.seriesData;
-        this.updateFlag=true;
-    console.log(this.seriesData,this.chartOptions.series)});
- 
-    
-  
-//   console.log(data1,this.seriesData);
+  seriesData;
+  topicsArray;
+  consumerArray;
+  // name="d dsvbdshbvhjvhdvs";
+  constructor(
+    private ds: DataService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
-}
-ngOnChanges(){
+  nameToDisplay;
+  ngOnInit(): void {
+    console.log(this.name);
+    this.topicsArray = Array.from(new Set(Data.map(rData => rData.topic)));
+    this.topicsArray = Array.from(
+      new Set(Data.map(rData => rData.consumer))
+    ).sort();
+    // this.seriesData = this.getProcessedArrivalData1(Data);
+
+    this.nameToDisplay = this.route.snapshot.paramMap.get("id");
+
+    this.ds.get(this.nameToDisplay + ".json", null).subscribe(data => {
+      this.seriesData = this.getProcessedArrivalData2(data);
+      this.chartOptions.series = this.seriesData;
+      this.updateFlag = true;
+    });
+
+    // this.name$ = this.route.paramMap.pipe(
+    //   switchMap((params: ParamMap) =>{
+    //      params.get('id');
+
+    //   })).subscribe(param=>{
+    //       console.log(param);
+    //       this.seriesData = this.getProcessedArrivalData2(data)
+    //       this.chartOptions.series = this.seriesData;
+    //       this.updateFlag=true;
+    //   console.log(this.seriesData,this.chartOptions.series)});
+
+    //   console.log(data1,this.seriesData);
+  }
+  ngOnChanges() {
     // console.log(this.HighchartsData);
-}
-// getProcessedArrivalData1(rawData) {
-//   let processedSeriesData = [];
-//   const self = this;
-//   rawData.forEach(element => {
-//     let index = processedSeriesData.findIndex(psData => psData.name === element.topic);
-//     if (index === -1) {
-//       processedSeriesData.push({ name: element.topic, data: [] });
-//       index = processedSeriesData.length - 1;
-//     }
-//     processedSeriesData[index]["data"].push({
-//       x: new Date(element.timestamp).getTime(),
-//       y: this.topicsArray.indexOf(element.consumer)
-//     })
-//   });
-//   return processedSeriesData;
-// }
-getProcessedArrivalData2(rawData) {
+  }
+  // getProcessedArrivalData1(rawData) {
+  //   let processedSeriesData = [];
+  //   const self = this;
+  //   rawData.forEach(element => {
+  //     let index = processedSeriesData.findIndex(psData => psData.name === element.topic);
+  //     if (index === -1) {
+  //       processedSeriesData.push({ name: element.topic, data: [] });
+  //       index = processedSeriesData.length - 1;
+  //     }
+  //     processedSeriesData[index]["data"].push({
+  //       x: new Date(element.timestamp).getTime(),
+  //       y: this.topicsArray.indexOf(element.consumer)
+  //     })
+  //   });
+  //   return processedSeriesData;
+  // }
+  getProcessedArrivalData2(rawData) {
     console.log(rawData);
-    if (isUndefined(rawData)){
-        return [];
+    if (isUndefined(rawData)) {
+      return [];
     }
-  let processedSeriesData = [];
-  const self = this;
-  let data3=rawData['samples']
-  processedSeriesData.push({name:rawData['topic'],data:[]})
-  data3.forEach(element => {
-    let p= Number(moment(element.timeStamp, "DD-MM-YYYY HH:mm:ss").format("x"))
-    processedSeriesData[0]["data"].push({
-      x: p,
-      y: element.arrivalRate
-    })
-  });
-  return processedSeriesData;
-}
-title = 'demo1';
-Highcharts = Highcharts; // required
-chartConstructor = 'chart'; // optional string, defaults to 'chart'
-chartOptions = {
-  chart: {
-    type: 'spline',
-  //   animation: Highcharts.svg, // don't animate in old IE
-    marginRight: 10,
-    events: {
-        load: function () {
+    let processedSeriesData = [];
+    const self = this;
+    let data3 = rawData["samples"];
+    processedSeriesData.push({ name: rawData["topic"], data: [] });
+    data3.forEach(element => {
+      let p = Number(
+        moment(element.timeStamp, "DD-MM-YYYY HH:mm:ss").format("x")
+      );
+      processedSeriesData[0]["data"].push({
+        x: p,
+        y: element.arrivalRate
+      });
+    });
+    return processedSeriesData;
+  }
+  title = "demo1";
+  Highcharts = Highcharts; // required
+  chartConstructor = "chart"; // optional string, defaults to 'chart'
+  chartOptions = {
+    chart: {
+      type: "spline",
+      //   animation: Highcharts.svg, // don't animate in old IE
+      marginRight: 10,
+      events: {
+        load: function() {
+          // set up the updating of the chart each second
+          var series = this.series[0];
 
-            // set up the updating of the chart each second
-            var series = this.series[0];
-            
-            setInterval(function () {
-                if (!series){
-                return 
-                }
-                var x = series['data'][series['data'].length-1]['x']+50000, // current time
-                    y = Math.random() * Math.floor(3);
-                  //   console.log(x,y)
-                series.addPoint([x, y], true, true);
-            }, 1000);
+          setInterval(function() {
+            if (!series) {
+              return;
+            }
+            var x = series["data"][series["data"].length - 1]["x"] + 50000, // current time
+              y = Math.random() * Math.floor(3);
+            //   console.log(x,y)
+            series.addPoint([x, y], true, true);
+          }, 1000);
         }
-    }
-},
-  time: {
-    useUTC: false
-},
-
-title: {
-    text: 'Live random data'
-},
-xAxis: {
-    type: 'datetime',
-    tickPixelInterval: 150
-},
-yAxis: {
-    title: {
-        text: 'Value'
+      }
     },
-    plotLines: [{
-        value: 0,
-        width: 1,
-        color: '#808080'
-    }]
-},
-tooltip: {
-    headerFormat: '<b>{series.name}</b><br/>',
-    pointFormat: '{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}'
-},
-legend: {
-    enabled: false
-},
-exporting: {
-    enabled: false
-},
-series: [
-//     {
-//     name: 'Random data',
-//     data: (function () {
-//         // generate an array of random data
-//         var data = [],
-//             time = (new Date()).getTime(),
-//             i;
+    time: {
+      useUTC: false
+    },
 
-//         for (i = -19; i <= 0; i += 1) {
-//             data.push({
-//                 x: time + i * 1000,
-//                 y: Math.random()
-//             });
-//         }
-//         return data;
-//     }())
-// }
-]
-}
-change(){
-    
-}
+    title: {
+      text: "Live random data"
+    },
+    xAxis: {
+      type: "datetime",
+      tickPixelInterval: 150
+    },
+    yAxis: {
+      title: {
+        text: "Value"
+      },
+      plotLines: [
+        {
+          value: 0,
+          width: 1,
+          color: "#808080"
+        }
+      ]
+    },
+    tooltip: {
+      headerFormat: "<b>{series.name}</b><br/>",
+      pointFormat: "{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}"
+    },
+    legend: {
+      enabled: false
+    },
+    exporting: {
+      enabled: false
+    },
+    series: [
+      //     {
+      //     name: 'Random data',
+      //     data: (function () {
+      //         // generate an array of random data
+      //         var data = [],
+      //             time = (new Date()).getTime(),
+      //             i;
+      //         for (i = -19; i <= 0; i += 1) {
+      //             data.push({
+      //                 x: time + i * 1000,
+      //                 y: Math.random()
+      //             });
+      //         }
+      //         return data;
+      //     }())
+      // }
+    ]
+  };
+  change() {}
 
-
-updateFlag = false; // optional boolean
-oneToOneFlag = true; // optional boolean, defaults to false
-
+  updateFlag = false; // optional boolean
+  oneToOneFlag = true; // optional boolean, defaults to false
 }
